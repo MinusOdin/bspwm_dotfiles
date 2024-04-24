59dfb34f6fe60d501bdb3d01a1e43cd0b59cbdc8
Arnau Belmonte Juliachs <arnau@debian.local.com>
2024-04-24 11:52:22 +0200

    dotfiles

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar top -c /home/arnau/.config/polybar/config.ini 
